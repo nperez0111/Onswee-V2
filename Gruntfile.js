9a14b7d45687ea85028f83f9d6ce57166ca068be
Nick Perez <nperez0111@gmail.com>
2015-02-01 21:33:01 -0500

    reupdate grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -74,7 +74,7 @@ module.exports = function (grunt) {
                 open: true,
                 livereload: 35729,
                 // Change this to '0.0.0.0' to access the server from outside
-                hostname: '10.0.0.2'
+                hostname: '0.0.0.0'
             },
             livereload: {
                 options: {
